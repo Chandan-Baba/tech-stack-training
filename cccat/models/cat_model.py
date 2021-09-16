58f9a1d019115f6bf20d928c7214c84443ab9d16
chandan-baba <cbaba20@gmail.com>
2021-09-16 11:06:21 +0530

    draft changes

--- cccat/models/cat_model.py
+++ cccat/models/cat_model.py
@@ -174,18 +174,17 @@ def cat_summary_from_bson(cat: BSONDocument) -> dto.CatSummary:
 def partial_update_cat_metadata(
     cat_id: dto.CatID,
     partial_update: dto.PartialUpdateCat,
-) -> dto.BulkUpdateResult:
+) -> dto.PartialUpdateCat:
 
     collection = get_collection(_COLLECTION_NAME)
     # import pdb
 
     # pdb.set_trace()
 
-    print("in model pu", partial_update.url)
+    # print("in model pu", partial_update.url)
     update = {"$set": {"url": partial_update.url}}
     results = collection.update_many({"cat_id": str(cat_id)}, update)
-    print(results)
-    return dto.BulkUpdateResult(
-        matched_count=results.matched_count,
-        modified_count=results.modified_count,
-    )
+    # print(results)
+
+    ##TODO here is asyncio throwing error.
+    return dto.PartialUpdateCat(url=results.url)
